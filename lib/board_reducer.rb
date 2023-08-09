f8c32dfd6f2fb20e8a06c0431225451afd483fb9
Alex Griffith <alexalexgriffith@gmail.com>
2023-08-09 15:18:28 -0400

    move Board::History to new file

--- lib/board_reducer.rb
+++ lib/board_reducer.rb
@@ -3,7 +3,7 @@ class Board::Reducer
   
   def initialize(board)
     @board = board
-    @history = History.new
+    @history = Board::History.new
     dispatch(Action.new(type: Action::INIT))
   end
 
@@ -111,53 +111,6 @@ class Board::Reducer
       end
     else
       state
-    end
-  end
-end
-
-class History
-  def initialize()
-    @history = []
-  end
-
-  def <<(next_action)
-    @history << next_action
-  end
-
-  def [](index)
-    @history[index]
-  end
-
-  def all
-    @history.dup
-  end
-
-  def last(n=1)
-    @history.last(n)
-  end
-
-  def length
-    @history.length
-  end
-
-  def find(**kwargs)
-    where(**kwargs).first
-  end
-
-  def after_id(action_id)
-    start_index = @history.index { |action| action.id == action_id }
-    @history[start_index..-1]
-  end
-
-  def where(**kwargs)
-    @history.select do |action|
-      kwargs.all? do |key, val|
-        if val == nil
-          !action.respond_to?(key) || !action.send(key)
-        else
-          action.respond_to?(key) ? action.send(key) == val : false
-        end
-      end
     end
   end
 end
