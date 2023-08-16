8fea5229183bccf04e5053fe60e65786c8d593d5
Alex Griffith <alexalexgriffith@gmail.com>
2023-08-15 20:59:46 -0400

    some metaprogramming

--- lib/solve_summary.rb
+++ lib/solve_summary.rb
@@ -1,4 +1,17 @@
 class Solve::Summary
+  Strategy::BaseStrategy.subclasses.each do |strategy|
+    define_method(:"solved_by_#{strategy.name}_cell_count") do
+      history.where(solves: true, strategy: strategy.name).length
+    end
+
+    define_method(:"num_#{strategy.name}s") do
+      history.where(
+        strategy: strategy.name,
+        type: Action::UPDATE_CELL,
+      ).map(&:strategy_id).uniq.length
+    end
+  end
+
   attr_reader :history, :strategies_used
 
   def initialize(history, strategies_used)
@@ -10,14 +23,14 @@ class Solve::Summary
     [
       "Solved: #{solved_status}",
       "Cells filled at start: #{initial_filled_cell_count}",
-      "Cells initially solveable 'by sudoku' (Naked single): #{naked_single_cell_count}",
-      used_strategy?(Strategy::HiddenSingle) && "Hidden singles: #{hidden_single_cell_count}",
+      "Cells initially solveable 'by sudoku' (Naked single): #{solved_by_naked_single_cell_count}",
+      used_strategy?(Strategy::HiddenSingle) && "Hidden singles: #{solved_by_hidden_single_cell_count}",
       used_strategy?(Strategy::NakedPair) && "Naked pairs: #{num_naked_pairs}",
       used_strategy?(Strategy::NakedPair) && "  cells solved 'by sudoku' after identifying naked pair: #{solved_by_naked_pair_cell_count}",
-      used_strategy?(Strategy::LockedCandidatesPointing) && "Locked lines: #{num_locked_pointing_lines}",
-      used_strategy?(Strategy::LockedCandidatesPointing) && "  cells solved 'by sudoku' after locked line in box: #{solved_by_locked_pointing_cell_count}",
-      used_strategy?(Strategy::LockedCandidatesClaiming) && "Claimed lines: #{num_claimed_lines}",
-      used_strategy?(Strategy::LockedCandidatesClaiming) && "  cells solved 'by sudoku' after identifying 'claiming' line/box: #{solved_by_claiming_lines_cell_count}",
+      used_strategy?(Strategy::LockedCandidatesPointing) && "Locked lines: #{num_locked_candidates_pointings}",
+      used_strategy?(Strategy::LockedCandidatesPointing) && "  cells solved 'by sudoku' after locked line in box: #{solved_by_locked_candidates_pointing_cell_count}",
+      used_strategy?(Strategy::LockedCandidatesClaiming) && "Claimed lines: #{num_locked_candidates_claimings}",
+      used_strategy?(Strategy::LockedCandidatesClaiming) && "  cells solved 'by sudoku' after identifying 'claiming' line/box: #{solved_by_locked_candidates_claiming_cell_count}",
       used_strategy?(Strategy::NakedTriple) && "Naked triples: #{num_naked_triples}",
       used_strategy?(Strategy::NakedTriple) && "  cells solved 'by sudoku' after identifying a naked triple: #{solved_by_naked_triple_cell_count}",
       used_strategy?(Strategy::NakedQuadruple) && "Naked quadruples: #{num_naked_quadruples}",
@@ -26,7 +39,7 @@ class Solve::Summary
       used_strategy?(Strategy::HiddenPair) && "  cells solved 'by sudoku' after identifying a hidden pair: #{solved_by_hidden_pair_cell_count}",
       used_strategy?(Strategy::XWing) && "X-Wings: #{num_x_wings}",
       used_strategy?(Strategy::XWing) && "  cells solved 'by sudoku' after identifying an x-wing: #{solved_by_x_wing_cell_count}",
-      used_strategy?(Strategy::Swordfish) && "Swordfishes: #{num_swordfish}",
+      used_strategy?(Strategy::Swordfish) && "Swordfishes: #{num_swordfishs}",
       used_strategy?(Strategy::Swordfish) && "  cells solved 'by sudoku' after identifying a swordfish: #{solved_by_swordfish_cell_count}",
       used_strategy?(Strategy::HiddenTriple) && "Hidden triples: #{num_hidden_triples}",
       used_strategy?(Strategy::HiddenTriple) && "  cells solved 'by sudoku' after identifying a hidden triple: #{solved_by_hidden_triple_cell_count}",
@@ -42,11 +55,11 @@ class Solve::Summary
   def total_count
     (
       initial_filled_cell_count +
-      naked_single_cell_count +
-      hidden_single_cell_count +
+      solved_by_naked_single_cell_count +
+      solved_by_hidden_single_cell_count +
       solved_by_naked_pair_cell_count +
-      solved_by_locked_pointing_cell_count +
-      solved_by_claiming_lines_cell_count +
+      solved_by_locked_candidates_pointing_cell_count +
+      solved_by_locked_candidates_claiming_cell_count +
       solved_by_naked_triple_cell_count +
       solved_by_naked_quadruple_cell_count +
       solved_by_hidden_pair_cell_count +
@@ -72,134 +85,5 @@ class Solve::Summary
 
   def initial_filled_cell_count
     history.find(type: Action::NEW_BOARD_SYNC).initial_data.reject { |v| v == Cell::EMPTY }.length
-  end
-
-  def naked_single_cell_count
-    history.where(solves: true, strategy: Strategy::NakedSingle.name).length
-  end
-
-  def hidden_single_cell_count
-    history.where(solves: true, strategy: Strategy::HiddenSingle.name).length
-  end
-
-  def num_naked_pairs
-    history.where(
-      strategy: Strategy::NakedPair.name,
-      type: Action::UPDATE_CELL,
-    ).map(&:strategy_id).uniq.length
-  end
-
-  def solved_by_naked_pair_cell_count
-    history.where(solves: true, strategy: Strategy::NakedPair.name).length
-  end
-
-  def num_locked_pointing_lines
-    history.where(
-      strategy: Strategy::LockedCandidatesPointing.name,
-      type: Action::UPDATE_CELL,
-    ).map(&:strategy_id).uniq.length
-  end
-
-  def solved_by_locked_pointing_cell_count
-    history.where(solves: true, strategy: Strategy::LockedCandidatesPointing.name).length
-  end
-
-  def num_claimed_lines
-    history.where(
-      strategy: Strategy::LockedCandidatesClaiming.name,
-      type: Action::UPDATE_CELL,
-    ).map(&:strategy_id).uniq.length
-  end
-
-  def solved_by_claiming_lines_cell_count
-    history.where(solves: true, strategy: Strategy::LockedCandidatesClaiming.name).length
-  end
-
-  def num_naked_triples
-    history.where(
-      strategy: Strategy::NakedTriple.name,
-      type: Action::UPDATE_CELL,
-    ).map(&:strategy_id).uniq.length
-  end
-
-  def solved_by_naked_triple_cell_count
-    history.where(solves: true, strategy: Strategy::NakedTriple.name).length
-  end
-
-  def num_naked_quadruples
-    history.where(
-      strategy: Strategy::NakedQuadruple.name,
-      type: Action::UPDATE_CELL,
-    ).map(&:strategy_id).uniq.length
-  end
-
-  def solved_by_naked_quadruple_cell_count
-    history.where(solves: true, strategy: Strategy::NakedQuadruple.name).length
-  end
-
-  def num_hidden_pairs
-    history.where(
-      strategy: Strategy::HiddenPair.name,
-      type: Action::UPDATE_CELL
-    ).map(&:strategy_id).uniq.length
-  end
-
-  def solved_by_hidden_pair_cell_count
-    history.where(solves: true, strategy: Strategy::HiddenPair.name).length
-  end
-
-  def num_hidden_triples
-    history.where(
-      strategy: Strategy::HiddenTriple.name,
-      type: Action::UPDATE_CELL,
-    ).map(&:strategy_id).uniq.length
-  end
-
-  def solved_by_hidden_triple_cell_count
-    history.where(solves: true, strategy: Strategy::HiddenTriple.name).length
-  end
-
-  def num_x_wings
-    history.where(
-      strategy: Strategy::XWing.name,
-      type: Action::UPDATE_CELL,
-    ).map(&:strategy_id).uniq.length
-  end
-
-  def solved_by_x_wing_cell_count
-    history.where(solves: true, strategy: Strategy::XWing.name).length
-  end
-
-  def num_swordfish
-    history.where(
-      strategy: Strategy::Swordfish.name,
-      type: Action::UPDATE_CELL,
-    ).map(&:strategy_id).uniq.length
-  end
-
-  def solved_by_swordfish_cell_count
-    history.where(solves: true, strategy: Strategy::Swordfish.name).length
-  end
-
-  def num_y_wings
-    history.where(
-      strategy: Strategy::YWing.name,
-      type: Action::UPDATE_CELL,
-    ).map(&:strategy_id).uniq.length
-  end
-
-  def solved_by_y_wing_cell_count
-    history.where(solves: true, strategy: Strategy::YWing.name).length
-  end
-
-  def num_skyscrapers
-    history.where(
-      strategy: Strategy::Skyscraper.name,
-      type: Action::UPDATE_CELL,
-    ).map(&:strategy_id).uniq.length
-  end
-
-  def solved_by_skyscraper_cell_count
-    history.where(solves: true, strategy: Strategy::Skyscraper.name).length
   end
 end
